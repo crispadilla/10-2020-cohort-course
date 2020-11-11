ac80cde37ec54416fb3e2e0743768ba8233f13c3
Cris Padilla <crispadilla82@gmail.com>
2020-11-10 22:42:16 -0800

    Fixed the division by 0 bug

--- week-3/day-2/calculator/script.js
+++ week-3/day-2/calculator/script.js
@@ -1,31 +1,37 @@
 function arithmetic(operation) {
+  var result;
+
   if (operation == "add") {
     var leftOp = document.getElementById("leftOperandAdd").value;
     var rightOp = document.getElementById("rightOperandAdd").value;
-    var result = parseInt(leftOp, 10) + parseInt(rightOp, 10);
+    result = parseInt(leftOp, 10) + parseInt(rightOp, 10);
     document.getElementById("addResult").innerHTML = result;
   }
   if (operation == "multiply") {
     var leftOp = document.getElementById("leftOperandMul").value;
     var rightOp = document.getElementById("rightOperandMul").value;
-    var result = parseInt(leftOp, 10) * parseInt(rightOp, 10);
+    result = parseInt(leftOp, 10) * parseInt(rightOp, 10);
     document.getElementById("mulResult").innerHTML = result;
   }
   if (operation == "divide") {
     var leftOp = document.getElementById("leftOperandDiv").value;
     var rightOp = document.getElementById("rightOperandDiv").value;
-    var result = parseInt(leftOp, 10) / parseInt(rightOp, 10);
+    // Check division by 0
+    rightOp == 0
+      ? (result = "undefined")
+      : (result = parseInt(leftOp, 10) / parseInt(rightOp, 10));
     document.getElementById("divResult").innerHTML = result;
   }
   if (operation == "subtract") {
     var leftOp = document.getElementById("leftOperandSub").value;
     var rightOp = document.getElementById("rightOperandSub").value;
-    var result = parseInt(leftOp, 10) - parseInt(rightOp, 10);
+    result = parseInt(leftOp, 10) - parseInt(rightOp, 10);
     document.getElementById("subResult").innerHTML = result;
   }
 }
 
 function arithmeticOptimized(button) {
+  var result;
   var leftOp = button.nextElementSibling.value; // left Operand
   var rightOp =
     button.nextElementSibling.nextElementSibling.nextElementSibling.value;
@@ -33,16 +39,19 @@ function arithmeticOptimized(button) {
 
   switch (operand) {
     case "+":
-      var result = parseInt(leftOp, 10) + parseInt(rightOp, 10);
+      result = parseInt(leftOp, 10) + parseInt(rightOp, 10);
       break;
     case "*":
-      var result = parseInt(leftOp, 10) * parseInt(rightOp, 10);
+      result = parseInt(leftOp, 10) * parseInt(rightOp, 10);
       break;
     case "/":
-      var result = parseInt(leftOp, 10) / parseInt(rightOp, 10);
+      // Check division by 0
+      rightOp == 0
+        ? (result = "undefined")
+        : (result = parseInt(leftOp, 10) / parseInt(rightOp, 10));
       break;
     case "-":
-      var result = parseInt(leftOp, 10) - parseInt(rightOp, 10);
+      result = parseInt(leftOp, 10) - parseInt(rightOp, 10);
       break;
   }
   // Print Result
@@ -50,6 +59,7 @@ function arithmeticOptimized(button) {
 }
 
 function arithmeticReoptimized(button) {
+  var result;
   var leftOp = button.parentElement.getElementsByClassName("leftOp")[0].value;
   var rightOp = button.parentElement.getElementsByClassName("rightOp")[0].value;
   var operand = button.parentElement.getElementsByClassName("operand")[0]
@@ -57,16 +67,19 @@ function arithmeticReoptimized(button) {
 
   switch (operand) {
     case "+":
-      var result = parseInt(leftOp, 10) + parseInt(rightOp, 10);
+      result = parseInt(leftOp, 10) + parseInt(rightOp, 10);
       break;
     case "*":
-      var result = parseInt(leftOp, 10) * parseInt(rightOp, 10);
+      result = parseInt(leftOp, 10) * parseInt(rightOp, 10);
       break;
     case "/":
-      var result = parseInt(leftOp, 10) / parseInt(rightOp, 10);
+      // Check division by 0
+      rightOp == 0
+        ? (result = "undefined")
+        : (result = parseInt(leftOp, 10) / parseInt(rightOp, 10));
       break;
     case "-":
-      var result = parseInt(leftOp, 10) - parseInt(rightOp, 10);
+      result = parseInt(leftOp, 10) - parseInt(rightOp, 10);
       break;
   }
   // Print Result
@@ -74,13 +87,17 @@ function arithmeticReoptimized(button) {
 }
 
 function arithmeticPro(button) {
+  var result;
   var leftOp = button.parentElement.getElementsByClassName("leftOp")[0].value;
   var rightOp = button.parentElement.getElementsByClassName("rightOp")[0].value;
   var operand = button.parentElement.getElementsByClassName("operand")[0]
     .innerHTML;
-  var result = eval(
-    `parseInt(${leftOp}, 10) ${operand} parseInt(${rightOp}, 10)`
-  );
+  // Check division by 0
+  operand == "/" && rightOp == 0
+    ? (result = "undefined")
+    : (result = eval(
+        `parseInt(${leftOp}, 10) ${operand} parseInt(${rightOp}, 10)`
+      ));
   // Print Result
   button.parentElement.getElementsByClassName("result")[0].innerHTML = result;
 }
